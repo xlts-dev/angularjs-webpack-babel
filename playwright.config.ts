f56edbd96e3f1a54b69a45916f3b58549ab662dd
Jaroslaw Zolnowski <jaroslaw.zolnowski@gmail.com>
2023-03-15 12:19:36 +0100

    fix(e2e): increase timeout to 1min

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ const config: PlaywrightTestConfig = {
   testDir: './e2e/tests',
   testMatch: /.e2e.js/,
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
